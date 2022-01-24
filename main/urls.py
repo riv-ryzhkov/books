fca89866ad226206f243fc51b76d6382ce3c9ec8
riv-ryzhkov <riv.ryzhkov@gmail.com>
2022-01-25 00:22:10 +0200

    My Books app add admin, 6th commit

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about-us', views.about, name='about'),
     path('create', views.create, name='create'),
     path('contacts', views.contacts, name='contacts'),
+    # path('admin', views.admin, name='admin'),
 ]
